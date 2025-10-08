0de6fc5106758f81086bab5d58a9a0988dccf4a7
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-10-09 00:06:45 +0500

    init styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import reactLogo from './assets/react.svg'
+import './app/styles/index.scss';
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='app'>
+      HEllo
+    </div>
   );
 }
 
