6c86a4f04991472777971f072125e149ed196dfd
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-12-16 20:54:36 +0500

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import App from 'App/*';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from './app/providers/ThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 );
